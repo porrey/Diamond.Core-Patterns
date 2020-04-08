ed06a4e5ed347cae6eed0a160519b2682415c761
Harshit Gindra <rr221100@prscoad.com>
2020-04-08 10:22:40 -0500

    Adding new project interfaces and factory for model validation rules and respective implementation

---
+++ Src/Diamond.Patterns.Abstractions/Factory/IModelRuleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Diamond.Patterns.Abstractions
+{
+    public interface IModelRuleFactory
+    {
+        /// <summary>
+        /// Get all model rule instances registered based on TInterface
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <returns>list of IModelRule instances</returns>
+        Task<IEnumerable<IModelRule<TInterface>>> GetAllAsync<TInterface>();
+
+        /// <summary>
+        /// Get all model rule instances registered based on TInterface and group name
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <returns>list of IModelRule instances</returns>
+        Task<IEnumerable<IModelRule<TInterface>>> GetAllAsync<TInterface>(string group);
+    }
+}
---
+++ Src/Diamond.Patterns.Abstractions/ModelRule/IModelRule.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Diamond.Patterns.Abstractions
+{
+    public interface IModelRule<TEntity>
+    {
+        /// <summary>
+        /// Validate model based on the defined rule asynchronously
+        /// </summary>
+        /// <param name="model">TEntity to be validated</param>
+        /// <returns>bool and consolidated error message</returns>
+        Task<(bool, string)> ValidateModelAsync(TEntity model);
+        /// <summary>
+        /// Group name to distinguish between different rules
+        /// </summary>
+        string Group { get; set; }
+    }
+}
